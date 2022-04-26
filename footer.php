16b3072b06eecf89ab4a6cf7a0fa5aba3031b691
Katrobin <100463891+Katrobin@users.noreply.github.com>
2022-04-26 19:41:54 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,15 +1,19 @@
 <footer class="container-fluid text-center">
   <div class="container">
     <div class="row">
-      <div class="col-md-6">
-        <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span></a>
-      </div>
-      <div class="col-6-md">
-        <p>Terms and Conditions</p>
-      </div>
-    </div>
-  </div>
-</footer>
-<?php wp_footer(); ?>
+      <div class="col-md-6 widgetleft">
+        <!-- <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span></a> -->
+        <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-left') ) : 
+endif; ?>
+</div>
+<div class="col-md-6">
+  <!-- <p>Terms and Conditions</p> -->
+  <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-right') ) : 
+endif; ?>
+  </div><!-- row-->
+  </div><!-- container -->
+  </div><!-- container-fluid-->
+  </footer>
+  <?php wp_footer(); ?>
 </body>
 </html>
